dbb766a57a947e728e9ba3bf19b3cdfe69906227
tomaszwasik31 <80346266+tomaszwasik31@users.noreply.github.com>
2023-07-26 12:18:33 +0100

    adding noise form app component not DevLink

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,12 @@
-import {
-  GlobalStyles,
-  Navbar,
-
-  SectionCta,
-  SectionManifesto,
-} from "@/devlink";
+import { GlobalStyles, Navbar, SectionCta, SectionManifesto } from "@/devlink";
+import NoiseC from "@/components/NoiseC";
 
 export default function Home() {
   return (
     <>
       <div className="page-wrapper">
-        <GlobalStyles/>
-        <div className="noise"></div>
+        <GlobalStyles />
+        <NoiseC />
         <div className="main-wrapper">
           <Navbar />
           <SectionManifesto />
---
+++ components/NoiseC.jsx
@@ -0,0 +1,3 @@
+export default function NoiseC() {
+  return <div className="noise"></div>;
+}
